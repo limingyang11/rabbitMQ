7af61987ad57515fa6685955cfd551ebe1175d3c
limingyang11 <977980062@qq.com>
2021-08-17 00:26:57 +0800

    update

---
+++ rabbitretry/src/test/java/com/lmy/rabbitretry/RabbitretryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmy.rabbitretry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitretryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
